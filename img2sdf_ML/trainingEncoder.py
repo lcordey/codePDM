a4a23a7179e148d3316ecec0681d91d3b4d5b548
Loïc Cordey <loic.cordey@gmail.com>
2021-10-20 16:15:13 +0200

    encoder rework grid

--- img2sdf_ML/trainingEncoder.py
+++ img2sdf_ML/trainingEncoder.py
@@ -135,8 +135,8 @@ for epoch in range(num_epoch):
     # for batch_front, batch_left, batch_back, batch_right, batch_top, batch_target_code in training_generator_grid:
 
 
-        print(f"time total time: {time.time() - start_time}")
-        start_time = time.time()
+        # print(f"total time: {time.time() - start_time}")
+        # start_time = time.time()
 
         optimizer.zero_grad()
 
@@ -156,7 +156,7 @@ for epoch in range(num_epoch):
         print("epoch: {}, L2 loss: {:.5f}, L1 loss: {:.5f} mean abs pred: {:.5f}, mean abs target: {:.5f}, LR: {:.6f}".format(epoch, torch.Tensor(log_loss[-10:]).mean(), \
         abs(pred_vecs - target_code).mean(), abs(pred_vecs).mean(), abs(target_code).mean(), optimizer.param_groups[0]['lr']  ))
 
-        print(f"network time: {time.time() - start_time}")
+        # print(f"network time: {time.time() - start_time}")
 
     
     scheduler.step()
@@ -171,7 +171,7 @@ for epoch in range(num_epoch):
 #     abs(pred_vecs - target_vecs.unsqueeze(1).repeat(1, num_validation_image_per_scene, 1)).mean(), abs(pred_vecs).mean(), abs(target_vecs[batch_scene_idx]).mean()))
 
 
-
+print(f"time for training: {time.time() - start_time}")
 
 
 #save model
