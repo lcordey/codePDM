ccdb3a8f1fbbd8dc449cd624bdd616e89841c0ce
Loïc Cordey <loic.cordey@gmail.com>
2021-10-21 16:50:19 +0200

    real vgg grid

--- img2sdf_ML/networks.py
+++ img2sdf_ML/networks.py
@@ -37,7 +37,7 @@ def conv_block2D(in_list, out_list, k_list, p_list, pooling_k):
 def fc_layer(size_in, size_out):
     layer = nn.Sequential(
         nn.Linear(size_in, size_out),
-        # nn.BatchNorm1d(size_out),
+        nn.BatchNorm1d(size_out),
         nn.ReLU()
     )
     return layer
