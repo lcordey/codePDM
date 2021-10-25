142d431c9f4bd816bab50d1162dd9b5c7809c9c8
Loïc Cordey <loic.cordey@gmail.com>
2021-10-25 15:09:33 +0200

    test sdf loss validation

--- img2sdf_ML/trainingEncoder.py
+++ img2sdf_ML/trainingEncoder.py
@@ -240,8 +240,8 @@ if NEWTORK == 'grid':
                     pred_vecs_validation = encoder(input_im_validation)
                     loss_pred_validation.append(loss(pred_vecs_validation, target_code_validation).detach().cpu())
 
-                    sdf_validation = decoder(pred_vecs_validation)
-                    sdf_target= decoder(target_code_validation)
+                    sdf_validation = decoder(pred_vecs_validation,xyz)
+                    sdf_target= decoder(target_code_validation,xyz)
 
                     # assign weight of 0 for easy samples that are well trained
                     threshold_precision = 1/resolution
