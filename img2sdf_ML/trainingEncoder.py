fbd3d246007018df09b11e5d8e14676ef0548e9b
Loïc Cordey <loic.cordey@gmail.com>
2021-10-27 13:45:26 +0200

    test encoder new images

--- img2sdf_ML/trainingEncoder.py
+++ img2sdf_ML/trainingEncoder.py
@@ -426,7 +426,7 @@ elif NEWTORK == 'face':
                     scene_id = 0
                     for batch_front, batch_left, batch_back, batch_right, batch_top, batch_target_code in validation_generator_face:
                         front, left, back, right, top, target_code_validation = batch_front.cuda(), batch_left.cuda(), batch_back.cuda(), batch_right.cuda(), batch_top.cuda(), batch_target_code.cuda()
-                        pred_vecs_validation = encoder(front, left, back, right, top)
+                        pred_vecs_validation = encoder(front, left, back, right, top).detach()
 
                         pred_vecs_matrix[scene_id, epoch_validation, :] = pred_vecs_validation
                         # loss_pred_validation.append(loss(pred_vecs_validation, target_code_validation))
