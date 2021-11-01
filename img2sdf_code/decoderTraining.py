2f6f9ddea4d5d6e41f1d496f688f047f5c93c2b8
Loïc Cordey <loic.cordey@gmail.com>
2021-11-01 17:14:54 +0100

    decoder with 2 model per batch

--- img2sdf_code/decoderTraining.py
+++ img2sdf_code/decoderTraining.py
@@ -240,7 +240,7 @@ if __name__ == '__main__':
             pred_rgb = pred[:,1:]
 
             loss_sdf, loss_rgb, loss_kl = compute_loss(pred_sdf, pred_rgb, sdf_gt.reshape(mini_batch_size * num_samples_per_model), rgb_gt.reshape(mini_batch_size * num_samples_per_model, 3), threshold_precision, param)
-            loss_sdf, loss_rgb, loss_kl = compute_loss(pred_sdf_slice, pred_rgb_slice, sdf_gt.reshape(mini_batch_size * num_samples_per_model), rgb_gt.reshape(mini_batch_size * num_samples_per_model, 3), threshold_precision, param)
+            # loss_sdf, loss_rgb, loss_kl = compute_loss(pred_sdf_slice, pred_rgb_slice, sdf_gt.reshape(mini_batch_size * num_samples_per_model), rgb_gt.reshape(mini_batch_size * num_samples_per_model, 3), threshold_precision, param)
             
 
             # loss_total = loss_sdf + loss_rgb + loss_kl
