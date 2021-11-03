437036909b75b6f331ae80bd73aba7a84f8607d4
Loïc Cordey <loic.cordey@gmail.com>
2021-11-03 15:34:40 +0100

    reworking networks

--- img2sdf_code/networks.py
+++ img2sdf_code/networks.py
@@ -1,30 +1,33 @@
 from pickle import FALSE
 import torch.nn as nn
 import torch
+import math
 
 import IPython
 
 
-def conv_layer3D(chann_in, chann_out, k_size, p_size, batch_norm):
-    # layer = nn.Sequential(
-    #     nn.Conv3d(chann_in, chann_out, kernel_size=k_size, padding=p_size),
-    #     # nn.BatchNorm3d(chann_out),
-    #     nn.ReLU()
-    # )
+def conv_layer3D(chann_in, chann_out, k_size, p_size, batch_norm=False):
 
     layers = [nn.Conv3d(chann_in, chann_out, kernel_size=k_size, padding=p_size)]
     if batch_norm:
         layers += [nn.BatchNorm3d(chann_out)]
     layers += [nn.ReLU()]
-    # return layer
+
     return nn.Sequential(*layers)
 
 def conv_block3D(in_list, out_list, k_list, p_list, pooling_k, batch_norm=FALSE):
 
     layers = [ conv_layer3D(in_list[i], out_list[i], k_list[i], p_list[i], batch_norm) for i in range(len(in_list)) ]
     layers += [ nn.MaxPool3d(kernel_size = pooling_k)]
+
     return nn.Sequential(*layers)
 
+def features_extraction_conv_block3D(num_block, num_features, k_list, p_list, pooling_k, batch_norm=FALSE):
+    layers  = [conv_block3D([3, num_features], [num_features, num_features], k_list, p_list, pooling_k, batch_norm=batch_norm)]
+    for i in range(1, num_block):
+        layers += [conv_block3D([num_features, num_features], [num_features, num_features], k_list, p_list, pooling_k, batch_norm=batch_norm)]
+
+    return nn.Sequential(*layers)
 
 def fc_layer(chann_in, chann_out, batch_norm=False):
 
@@ -34,6 +37,12 @@ def fc_layer(chann_in, chann_out, batch_norm=False):
     layers += [nn.ReLU()]
 
     return nn.Sequential(*layers)
+
+def fc_block(num_fc_layer, num_features, num_features_extracted, latent_size, batch_norm=False):
+    layers = [fc_layer(num_features_extracted * num_features, num_features, batch_norm=batch_norm)]
+    for i in range(1, num_fc_layer):
+        layers += [fc_layer(num_features, num_features, batch_norm=batch_norm)]
+    layers += nn.Linear(num_features, latent_size)
 
 
 class Decoder(nn.Module):
@@ -74,26 +83,45 @@ class Decoder(nn.Module):
         return x 
 
 class EncoderGrid(nn.Module):
-    def __init__(self,latent_size, batch_norm_conv=False, batch_norm_fc=False):
+    def __init__(self,latent_size, param, batch_norm_conv=False, batch_norm_fc=False):
         super(EncoderGrid, self).__init__()
 
         features_encoder = 64
 
-        self.block1 = conv_block3D([3,features_encoder], [features_encoder,features_encoder], [3,3], [1,1], 2, batch_norm=batch_norm_conv)
-        self.block2 = conv_block3D([features_encoder,features_encoder], [features_encoder,features_encoder], [3,3], [1,1], 2, batch_norm=batch_norm_conv)
-        self.block3 = conv_block3D([features_encoder,2 * features_encoder], [2 * features_encoder, 2 * features_encoder], [3,3], [1,1], 2, batch_norm=batch_norm_conv)
+        # self.block1 = conv_block3D([3,features_encoder], [features_encoder,features_encoder], [3,3], [1,1], 2, batch_norm=batch_norm_conv)
+        # self.block2 = conv_block3D([features_encoder,features_encoder], [features_encoder,features_encoder], [3,3], [1,1], 2, batch_norm=batch_norm_conv)
+        # self.block3 = conv_block3D([features_encoder,2 * features_encoder], [2 * features_encoder, 2 * features_encoder], [3,3], [1,1], 2, batch_norm=batch_norm_conv)
 
-        self.fc1 = fc_layer(6*3*3*features_encoder * 2, features_encoder, batch_norm=batch_norm_fc)
-        self.fc2 = fc_layer(features_encoder, features_encoder, batch_norm=batch_norm_fc)
-        self.fc3 = fc_layer(features_encoder, features_encoder, batch_norm=batch_norm_fc)
-        self.fc4 = nn.Linear(features_encoder, latent_size)
+        self.features_extraction = features_extraction_conv_block3D(param["num_conv_layer"], features_encoder, [3,3], [1,1], 2, batch_norm=batch_norm_conv)
+
+        num_slice_features = math.floor(param["num_slices"] / 2**param["num_conv_layer"])
+        num_width_features = math.floor(param["width"] / 2**param["num_conv_layer"])
+        num_height_features = math.floor(param["height"] / 2**param["num_conv_layer"])
+
+        assert(num_slice_features > 0 and num_width_features > 0 and num_height_features > 0), "too many conv layers"
+
+        num_slice_features = math.floor(param["num_slices"] / 2**param["num_conv_layer"])
+        num_features_extracted = num_slice_features * num_width_features * num_height_features
+
+        print(f"Init Encoder, features size of: {num_slice_features} x {num_width_features} x {num_height_features}")
+
+        self.regression_from_features = fc_block(param["num_fc_layer"], features_encoder, num_features_extracted, latent_size, batch_norm=batch_norm_fc)
+
+        # self.fc1 = fc_layer(6*3*3*features_encoder * 2, features_encoder, batch_norm=batch_norm_fc)
+        # self.fc2 = fc_layer(features_encoder, features_encoder, batch_norm=batch_norm_fc)
+        # self.fc3 = fc_layer(features_encoder, features_encoder, batch_norm=batch_norm_fc)
+        # self.fc4 = nn.Linear(features_encoder, latent_size)
 
     def forward(self, image):
 
-        temp = self.block1(image)
-        temp = self.block2(temp)
-        features = self.block3(temp)
+        # temp = self.block1(image)
+        # temp = self.block2(temp)
+        # features = self.block3(temp)
 
-        latent_code = self.fc4(self.fc3(self.fc2(self.fc1(features.view(features.size(0), -1)))))
+        # latent_code = self.fc4(self.fc3(self.fc2(self.fc1(features.view(features.size(0), -1)))))
+
+        features = self.features_extraction(image)
+
+        latent_code = self.regression_from_features(features.view(features.size(0), -1))
 
         return latent_code
