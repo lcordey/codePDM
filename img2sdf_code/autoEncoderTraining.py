a0878da0a3e691907dac084ddac9adcf85f43348
Loïc Cordey <loic.cordey@gmail.com>
2021-11-05 11:34:34 +0100

    test autoenc

--- img2sdf_code/autoEncoderTraining.py
+++ img2sdf_code/autoEncoderTraining.py
@@ -220,6 +220,8 @@ if __name__ == '__main__':
             pred_sdf = pred[:,0]
             pred_rgb = pred[:,1:]
 
+            IPython.embed()
+
             loss_sdf, loss_rgb, loss_kl = compute_loss(pred_sdf, pred_rgb, sdf_gt, rgb_gt, predicted_code, threshold_precision, param_dec)
             # loss_sdf, loss_rgb, loss_kl = compute_loss(pred_sdf, pred_rgb, sdf_gt, rgb_gt, predicted_code, predicted_code_log_std, threshold_precision, param_dec)
 
