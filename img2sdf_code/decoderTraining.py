7b5b99a776fc950c0062121920b4805ef862295f
Loïc Cordey <loic.cordey@gmail.com>
2021-11-09 14:47:14 +0100

    decoder mega big test

--- img2sdf_code/decoderTraining.py
+++ img2sdf_code/decoderTraining.py
@@ -59,22 +59,22 @@ def init_lat_codes(num_scenes, latent_size):
 def init_opt_sched(decoder, lat_vecs_mu, lat_vecs_log_std, param):
     """ initialize optimizer and scheduler"""
 
-    optimizer = torch.optim.Adam(
-        [
-            {
-                "params": decoder.parameters(),
-                "lr": param["eta_decoder"],
-            },
-            {
-                "params": lat_vecs_mu.parameters(),
-                "lr": param["eta_latent_space_mu"],
-            },
-            {
-                "params": lat_vecs_log_std.parameters(),
-                "lr": param["eta_latent_space_std"],
-            },
-        ]
-    )
+    # optimizer = torch.optim.Adam(
+    #     [
+    #         {
+    #             "params": decoder.parameters(),
+    #             "lr": param["eta_decoder"],
+    #         },
+    #         {
+    #             "params": lat_vecs_mu.parameters(),
+    #             "lr": param["eta_latent_space_mu"],
+    #         },
+    #         {
+    #             "params": lat_vecs_log_std.parameters(),
+    #             "lr": param["eta_latent_space_std"],
+    #         },
+    #     ]
+    # )
 
     optimizer_decoder = torch.optim.Adam(
         [
@@ -97,12 +97,10 @@ def init_opt_sched(decoder, lat_vecs_mu, lat_vecs_log_std, param):
         ]
     )
 
-    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=param["gamma_decoder_LR"])
-    # scheduler_decoder = torch.optim.lr_scheduler.ExponentialLR(optimizer_decoder, gamma=param["gamma_decoder_LR"])
-    # scheduler_code = torch.optim.lr_scheduler.ExponentialLR(optimizer_code, gamma=param["gamma_code_LR"])
+    scheduler_decoder = torch.optim.lr_scheduler.ExponentialLR(optimizer_decoder, gamma=param["gamma_decoder_LR"])
+    scheduler_code = torch.optim.lr_scheduler.ExponentialLR(optimizer_code, gamma=param["gamma_code_LR"])
 
-    # return optimizer_decoder, optimizer_code, scheduler_decoder, scheduler_code
-    return optimizer, scheduler
+    return optimizer_decoder, optimizer_code, scheduler_decoder, scheduler_code
 
 def compute_time_left(time_start, samples_count, num_model, num_samples_per_model, epoch, num_epoch):
     """ Compute time left until the end of training """
@@ -230,8 +228,8 @@ if __name__ == '__main__':
     decoder = Decoder(param_all["latent_size"], batch_norm=True).cuda()
 
     # initialize optimizer and scheduler
-    optimizer, scheduler = init_opt_sched(decoder, lat_code_mu, lat_code_log_std, param["optimizer"])
-    # optimizer_decoder, optimizer_code, scheduler_decoder, scheduler_code = init_opt_sched(decoder, lat_code_mu, lat_code_log_std, param["optimizer"])
+    # optimizer, scheduler = init_opt_sched(decoder, lat_code_mu, lat_code_log_std, param["optimizer"])
+    optimizer_decoder, optimizer_code, scheduler_decoder, scheduler_code = init_opt_sched(decoder, lat_code_mu, lat_code_log_std, param["optimizer"])
 
     # logs
     logs = dict()
@@ -250,9 +248,9 @@ if __name__ == '__main__':
     for epoch in range (param["num_epoch"]):
         samples_count = 0
         for model_idx, sdf_gt, rgb_gt, xyz_idx in training_generator:
-            optimizer.zero_grad()
-            # optimizer_decoder.zero_grad()
-            # optimizer_code.zero_grad()
+        #     optimizer.zero_grad()
+            optimizer_decoder.zero_grad()
+            optimizer_code.zero_grad()
 
             # time_loading = time.time() - time_start
             # print(f"Time to load the data: {time_loading}")
@@ -281,9 +279,9 @@ if __name__ == '__main__':
 
             #update weights
             loss_total.backward()
-            optimizer.step()
-            # optimizer_decoder.step()
-            # optimizer_code.step()
+            # optimizer.step()
+            optimizer_decoder.step()
+            optimizer_code.step()
 
             # estime time left
             samples_count += batch_size
@@ -327,9 +325,9 @@ if __name__ == '__main__':
             # print(f"Time for network pass: {time.time() - time_start}")
             # time_start = time.time()
 
-        scheduler.step()
-        # scheduler_decoder.step()
-        # scheduler_code.step()
+        # scheduler.step()
+        scheduler_decoder.step()
+        scheduler_code.step()
 
     print(f"Training finish in {(int)((time.time() - time_start) / 60)} min")
 
