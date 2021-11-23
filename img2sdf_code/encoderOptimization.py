daca771e60b88ea59ef0aff98694e4e9a3db3e6a
Loïc Cordey <loic.cordey@gmail.com>
2021-11-23 18:32:07 +0100

    test optim encoder after pred

--- img2sdf_code/encoderOptimization.py
+++ img2sdf_code/encoderOptimization.py
@@ -128,7 +128,9 @@ def get_code_from_grid(grid, latent_size):
     return lat_code
 
 
-
+print(torch.cuda.get_device_properties(0).total_memory)
+print(torch.cuda.memory_reserved(0))
+print(torch.cuda.memory_allocated(0))
 
 
 # load parameters
